5394d0f6b0cf62f371c5f478523be231f7125a9e
Matthew LaMeyer <matthew.lameyer@gmail.com>
2015-11-27 10:49:27 -0600

    add catdir and changed cat

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Debug=pa4ics462
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/pa4ics462
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/pa4ics462
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Debug=pa4ics462.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/pa4ics462.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/pa4ics462.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Release=pa4ics462
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/pa4ics462
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/pa4ics462
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Release=pa4ics462.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/pa4ics462.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/pa4ics462.tar
 #
 # include compiler specific variables
 #
